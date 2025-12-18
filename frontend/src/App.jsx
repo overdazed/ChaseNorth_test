ac0a8865331de59b1af7b2f46a50a2a1cf893e2d
overdazed <svetlana-pantschenko@web.de>
2025-12-18 15:12:04 +0100

    Auto-commit: 2025-12-18 15:12:03

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-// LAST WORKING: 2025-12-18 14:47
+// LAST WORKING: 2025-12-18 15:12
 import React from 'react'
 import { BrowserRouter, Route, Routes, useLocation, useNavigationType } from "react-router-dom";
 import { useEffect, useRef } from 'react';
