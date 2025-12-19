c9e7581669c8e4207f4278c143af4a105b8023a2
overdazed <svetlana-pantschenko@web.de>
2025-12-19 19:21:45 +0100

    Auto-commit: 19/12/2025 19:21:45,17

--- frontend/src/components/Products/Bento.jsx
+++ frontend/src/components/Products/Bento.jsx
@@ -1,9 +1,8 @@
-"use client";
 import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { LayoutGrid } from "../ui/layout-grid";
 import mensCollectionImage from "../../assets/man/winter.jpg";
-import womensCollectionImage from "../../assets/woman/winter.jpg";
+import womensCollectionImage from "../../assets/woman/wi";
 import topCollectionImage from "../../assets/top/winter.jpg";
 import bottomCollectionImage from "../../assets/bottom/winter-2.jpg";
 import {useNavigate} from "react-router-dom";
@@ -24,7 +23,7 @@ const SkeletonOne = ({ onClick }) => {
             style={{ pointerEvents: 'auto' }}
         >
             {/* Dim overlay with reduced opacity that becomes fully transparent on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:opacity-0 transition-opacity duration-500 z-0"></div>
+            <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-500 z-0"></div>
 
             {/* Content */}
             <div className="absolute bottom-10 left-6 z-10 space-y-4">
@@ -79,12 +78,12 @@ const SkeletonTwo = ({ onClick }) => {
             style={{ pointerEvents: 'auto' }}
         >
             {/* Dim overlay with reduced opacity that becomes fully transparent on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:opacity-0 transition-opacity duration-500 z-0"></div>
+            <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-500 z-0"></div>
 
             {/* Content - Right Aligned */}
             <div className="absolute bottom-10 right-4 z-10 space-y-4 text-right">
                 <p className="font-bold text-white text-4xl transform translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out">
-                    House above the clouds
+                    Top
                 </p>
                 <p className="font-normal text-base text-white transform translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out delay-100">
                     Perched high above the world, this house offers breathtaking views and a
@@ -111,7 +110,7 @@ const SkeletonThree = ({ onClick }) => {
             style={{ pointerEvents: 'auto' }}
         >
             {/* Dim overlay with reduced opacity that becomes fully transparent on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:opacity-0 transition-opacity duration-500 z-0"></div>
+            <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-500 z-0"></div>
 
             {/* Content */}
             <div className="absolute top-10 left-5 z-10 space-y-4">
@@ -142,12 +141,12 @@ const SkeletonFour = ({ onClick }) => {
             style={{ pointerEvents: 'auto' }}
         >
             {/* Dim overlay with reduced opacity that becomes fully transparent on hover */}
-            <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:opacity-0 transition-opacity duration-500 z-0"></div>
+            <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 transition-opacity duration-500 z-0"></div>
 
             {/* Content - Right Aligned */}
             <div className="absolute top-10 right-4 z-10 space-y-4 text-right">
                 <p className="font-bold text-white text-4xl transform translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out">
-                    House above the clouds
+                    Men
                 </p>
                 <p className="font-normal text-base text-white transform translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out delay-100">
                     Perched high above the world, this house offers breathtaking views and a
