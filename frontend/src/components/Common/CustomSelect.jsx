6439974d40fe1e0cd25fdca88084b41608a66f61
overdazed <svetlana-pantschenko@web.de>
2025-12-19 12:51:44 +0100

    Auto-commit: 19/12/2025 12:51:43,96

--- frontend/src/components/Common/CustomSelect.jsx
+++ frontend/src/components/Common/CustomSelect.jsx
@@ -76,7 +76,7 @@ const StyledWrapper = styled.div`
     font-size: 0.875rem; /* text-sm */
   }
 
-  /* Day mode styles (default) */
+  /* Day Mode Styles */
   .selected {
     background-color: rgb(250 250 250); /* bg-neutral-50 */
     border: 1px solid rgb(209 213 219); /* border-gray-300 */
@@ -145,38 +145,36 @@ const StyledWrapper = styled.div`
     background-color: rgb(239 246 255); /* bg-blue-50 */
     color: rgb(29 78 216); /* text-blue-700 */
     font-weight: 600;
+    font-size: 0.9375rem;
+    line-height: 1.5;
   }
 
-  /* Dark mode styles */
-  .dark .selected {
+  /* Dark Mode Styles */
+  .dark .select .selected {
     background-color: rgb(55 65 81); /* dark:bg-gray-700 */
     border-color: rgb(75 85 99); /* dark:border-gray-600 */
     color: rgb(255 255 255); /* dark:text-white */
   }
 
-  .dark .options {
+  .dark .select .options {
     background-color: rgb(55 65 81); /* dark:bg-gray-700 */
     border-color: rgb(75 85 99); /* dark:border-gray-600 */
   }
 
-  .dark .option {
+  .dark .select .option {
     color: rgb(255 255 255); /* dark:text-white */
   }
 
-  .dark .option:hover {
+  .dark .select .option:hover {
     background-color: rgb(75 85 99); /* dark:hover:bg-gray-600 */
   }
 
-  .dark .select:focus-within .selected {
-    border-color: rgb(59 130 246); /* dark:focus:border-primary-500 */
-  }
-
-  .dark .option.selected {
+  .dark .select .option.selected {
     background-color: rgb(30 58 138); /* dark:bg-blue-900 */
     color: rgb(191 219 254); /* dark:text-blue-100 */
   }
 
-  .dark .arrow {
+  .dark .select .arrow {
     fill: rgb(250 250 250); /* text-neutral-50 */
   }
 `;
