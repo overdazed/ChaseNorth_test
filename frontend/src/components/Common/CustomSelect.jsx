54690fcf25808a294381cf6dd01c45316750f4f1
overdazed <svetlana-pantschenko@web.de>
2025-12-19 14:21:44 +0100

    Auto-commit: 19/12/2025 14:21:44,10

--- frontend/src/components/Common/CustomSelect.jsx
+++ frontend/src/components/Common/CustomSelect.jsx
@@ -30,7 +30,7 @@ const CustomSelect = ({ options = [], value, onChange, placeholder = 'Select...'
   };
 
   const displayText = selectedValue ?
-      options.find(opt => opt.value === selectedValue)?.label : placeholder;
+      options.find(opt => opt.value === selectedValue)?.label : '';
 
   return (
       <StyledWrapper ref={selectRef}>
@@ -40,12 +40,14 @@ const CustomSelect = ({ options = [], value, onChange, placeholder = 'Select...'
               onClick={() => setIsOpen(!isOpen)}
               data-value={selectedValue}
           >
-            <span>{displayText}</span>
+            <span className={!selectedValue ? 'text-gray-400 dark:text-gray-400' : ''}>
+              {selectedValue ? displayText : placeholder}
+            </span>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 height="1em"
                 viewBox="0 0 512 512"
-                className="arrow"
+                className={`arrow ${!selectedValue ? 'text-gray-400 dark:text-gray-400' : ''}`}
             >
               <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
             </svg>
