6b163c8cc5aae7ced82f995770ff0c26c07c3348
overdazed <svetlana-pantschenko@web.de>
2025-12-20 01:31:45 +0100

    Auto-commit: 20/12/2025  1:31:45,27

--- backend/routes/subscribeRoute.js
+++ backend/routes/subscribeRoute.js
@@ -1,47 +1,47 @@
-// import express
-const express = require('express');
-const mongoose = require("mongoose");
-
-const router = express.Router();
-
-const Subscriber  = require("../models/Subscriber");
-
-// Add route
-// @route POST /api/subscribe
-// @desc Handle newsletter subscription
-// @access Public
-router.post('/subscribe', async (req, res) => {
-    // restructure email from the request body
-    const { email } = req.body;
-    // check if the email field is provided in the request body
-    if (!email) {
-        // If no email is provided respond with a 400 status code
-        return res.status(400).json({message: 'Email is required'});
-    }
-
-    try {
-        // Check if the email is already subscribed
-        let subscriber = await Subscriber.findOne({ email });
-        if (subscriber) {
-            // If the email is already in the database, respond with a 400 status code
-            return res.status(400).json({message: 'Email is already subscribed'});
-        }
-
-        // If email is not subscribed, create a new subscriber
-        subscriber = new Subscriber({
-            email
-        });
-        // Save the subscriber to the database
-        await subscriber.save();
-        // Send a 201 status code response
-        res.status(201).json({message: 'Successfully subscribed to the newsletter!'});
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: 'Server Error'});
-    }
-});
-
-module.exports = router
+// // import express
+// const express = require('express');
+// const mongoose = require("mongoose");
+//
+// const router = express.Router();
+//
+// const Subscriber  = require("../models/Subscriber");
+//
+// // Add route
+// // @route POST /api/subscribe
+// // @desc Handle newsletter subscription
+// // @access Public
+// router.post('/subscribe', async (req, res) => {
+//     // restructure email from the request body
+//     const { email } = req.body;
+//     // check if the email field is provided in the request body
+//     if (!email) {
+//         // If no email is provided respond with a 400 status code
+//         return res.status(400).json({message: 'Email is required'});
+//     }
+//
+//     try {
+//         // Check if the email is already subscribed
+//         let subscriber = await Subscriber.findOne({ email });
+//         if (subscriber) {
+//             // If the email is already in the database, respond with a 400 status code
+//             return res.status(400).json({message: 'Email is already subscribed'});
+//         }
+//
+//         // If email is not subscribed, create a new subscriber
+//         subscriber = new Subscriber({
+//             email
+//         });
+//         // Save the subscriber to the database
+//         await subscriber.save();
+//         // Send a 201 status code response
+//         res.status(201).json({message: 'Successfully subscribed to the newsletter!'});
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({message: 'Server Error'});
+//     }
+// });
+//
+// module.exports = router
 
 // open server.js file
 // include the following routes in server.js
