fa84631e8d2dca4f24117e16e2ee2a3efdb8c983
overdazed <svetlana-pantschenko@web.de>
2025-12-26 20:31:43 +0100

    Auto-commit: 26/12/2025 20:31:43,58

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,6 +5,7 @@ import { registerUser } from "../redux/slices/authSlice"
 import {useDispatch, useSelector} from "react-redux"
 import {mergeCart} from "../redux/slices/cartSlice";
 import styled from "styled-components";
+import {FaEye, FaEyeSlash} from "react-icons/fa6";
 
 const Register = () => {
 
@@ -14,6 +15,7 @@ const Register = () => {
     const [email, setEmail] = useState("")
 
     const [password, setPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
 
     // local state to control button loading
     const [isSigningUp, setIsSigningUp] = useState(false);
@@ -112,21 +114,51 @@ const Register = () => {
                     <div className="flex-column">
                         <label>Password</label>
                     </div>
-                    <div className="inputForm">
+                    <div className="inputForm" style={{ position: 'relative' }}>
                         <svg height={20} viewBox="-64 0 512 512" width={20} xmlns="http://www.w3.org/2000/svg">
                             <path d="M336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0" />
                             <path d="M304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0" />
                         </svg>
                         <input
-                            type="password"
+                            type={showPassword ? "text" : "password"}
                             className="input"
                             placeholder="Enter your Password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
+                            style={{ paddingRight: '40px' }}
                         />
-                        <svg className="pr-2" viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />
-                        </svg>
+                        <button 
+                            type="button" 
+                            onClick={() => setShowPassword(!showPassword)}
+                            style={{
+                                position: 'absolute',
+                                right: '10px',
+                                top: '50%',
+                                transform: 'translateY(-50%)',
+                                background: 'none',
+                                border: 'none',
+                                cursor: 'pointer',
+                                padding: '0',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            {/*{showPassword ? (*/}
+                            {/*    <svg viewBox="0 0 640 512" height="1em" xmlns="http://www.w3.org/2000/svg">*/}
+                            {/*        <path d="M38.8 5.1C28.4-3.1 13.3-1.2 5.1 9.2S-1.2 34.7 9.2 42.9l592 464c10.4 8.2 25.5 6.3 33.7-4.1s6.3-25.5-4.1-33.7L381.2 274.7c42.9-39.6 72.2-82.9 86.6-106.7 3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32c-68.2 0-130.8 22.4-181.2 60.1L38.8 5.1zM144 256c0 35.3 28.7 64 64 64h60.2L144 191.8v64.2zm0 0v64.2c0 35.3 28.7 64 64 64h160c25.9 0 48.6-15.5 58.2-38.6L436.6 416h-84.6c-10.2 0-18.4 8.3-18.4 18.4c0 6.5 3.4 12.5 9 15.9l84 58.8c5.6 3.9 12.9 3.1 17.8-1.9l39.6-39.6c4.9-4.9 4.9-12.9 0-17.9L38.8 5.1 9.2 34.7 144 169.5v86.5z" />*/}
+                            {/*    </svg>*/}
+                            {/*) : (*/}
+                            {/*    <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">*/}
+                            {/*        <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z" />*/}
+                            {/*    </svg>*/}
+                            {/*)}*/}
+                            {showPassword ? (
+                                <FaEye size={16} className="text-neutral-500 hover:text-black"/>
+                            ) : (
+                                <FaEyeSlash size={18} className="text-neutral-500 hover:text-black"/>
+                            )}
+                        </button>
                     </div>
                     <button type="submit" className="button-submit" disabled={isSigningUp}>
                         {isSigningUp ? 'Creating Account...' : 'Sign Up'}
