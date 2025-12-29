872d32ccc59c67209ac97a56ea407027052b5f69
overdazed <svetlana-pantschenko@web.de>
2025-12-29 14:11:44 +0100

    Auto-commit: 29/12/2025 14:11:44,72

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -63,64 +63,72 @@ const OrderConfirmationPage = () => {
     }
 
     return (
-        <div className="max-w-4xl mx-auto p-6 bg-white">
-            <h1 className="text-4xl font-bold text-center text-emerald-700 mb-8">Thank You For Your Order!</h1>
+        <div className="max-w-4xl mx-auto p-6 bg-neutral-50 dark:bg-neutral-950">
+            <h1 className="text-4xl font-bold text-center text-emerald-700 dark:text-emerald-500 mb-8">
+                Thank You For Your Order!
+            </h1>
 
-        {/*    If checkout data is present, display it */}
             {checkout && (
-                // add a div with class
-                <div className="p-6 rounded-lg border">
-                    <div className="flex justify-between mb-20">
-                        {/* Order Id and Date */}
+                <div className="p-6 rounded-lg shadow-md  bg-white dark:bg-neutral-800">
+                    <div className="flex flex-col sm:flex-row justify-between mb-8 sm:mb-20 gap-4">
                         <div>
-                            <h2 className="text-xl font-semibold">Order ID: {checkout._id}
+                            <h2 className="text-md sm:text-xl font-semibold text-neutral-900 dark:text-neutral-50">
+                                Order ID: {checkout._id}
                             </h2>
-                            <p className="text-neutral-500">
+                            <p className="text-neutral-500 dark:text-neutral-400 text-xs sm:text-md">
                                 Order date: {new Date(checkout.createdAt).toLocaleDateString()}
                             </p>
                         </div>
-                        {/* Estimated Delivery */}
-                        <div className="">
-                            <p className="text-emerald-700 text-sm">
-                                Estimated Delivery:{" "}{calculateEstimatedDelivery(checkout.createdAt)}
+                        <div>
+                            <p className="text-emerald-700 dark:text-emerald-400 text-xs sm:text-sm">
+                                Estimated Delivery: {calculateEstimatedDelivery(checkout.createdAt)}
                             </p>
                         </div>
                     </div>
-                    {/* Ordered Items */}
-                    <div className="mb-20">
-                        {/* look through the checkoutItems array */}
+
+                    <div className="mb-8 sm:mb-20 space-y-4">
                         {checkout.checkoutItems.map((item) => (
-                            <div key={item.productId} className="flex items-center mb-4">
+                            <div key={item.productId} className="flex items-center p-4 rounded-lg bg-neutral-50 dark:bg-neutral-700/50">
                                 <img
                                     src={item.image}
-                                    alt={item.name} className="w-16 h-16 object-cover rounded-md mr-4"
+                                    alt={item.name}
+                                    className="w-16 h-16 object-cover rounded-md mr-4"
                                 />
-                                <div>
-                                    <h4 className="text-md font-semibold">{item.name}</h4>
-                                    <p className="text-neutral-500 text-sm">{item.color} | {item.size}</p>
+                                <div className="flex-1">
+                                    <h4 className="text-md font-semibold text-neutral-900 dark:text-neutral-50">{item.name}</h4>
+                                    <p className="text-neutral-500 dark:text-neutral-300 text-sm">
+                                        {item.color} | {item.size}
+                                    </p>
                                 </div>
-                                <div className="ml-auto text-right">
-                                    <p className="text-md">${item.price}</p>
-                                    <p className="text-neutral-500 text-sm">Qty: {item.quantity}</p>
+                                <div className="ml-4 text-right">
+                                    <p className="text-md font-medium text-neutral-900 dark:text-neutral-50">${item.price}</p>
+                                    <p className="text-neutral-500 dark:text-neutral-300 text-sm">Qty: {item.quantity}</p>
                                 </div>
                             </div>
                         ))}
                     </div>
-                    {/* Payment and Delivery Info */}
-                    <div className="grid grid-cols-2 gap-8">
-                        {/* Payment Info */}
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                         <div>
-                            <h4 className="text-lg font-semibold mb-2">
+                            <h4 className="text-lg font-semibold mb-2 text-neutral-900 dark:text-neutral-50">
                                 Payment
                             </h4>
-                            <p className="text-neutral-600">PayPal</p>
+                            <p className="text-neutral-600 dark:text-neutral-300">PayPal</p>
                         </div>
 
-                        {/* Delivery Info */}
                         <div>
-                            <h4 className="text-lg font-semibold mb-2">Delivery</h4>
-                            <p className="text-neutral-600">{checkout.shippingAddress.address}</p>
-                            <p className="text-neutral-600">{checkout.shippingAddress.city},{" "}{checkout.shippingAddress.country}</p>
+                            <h4 className="text-lg font-semibold mb-2 text-neutral-900 dark:text-neutral-50">
+                                Delivery
+                            </h4>
+                            <p className="text-neutral-600 dark:text-neutral-300">
+                                {checkout.shippingAddress.firstName} {checkout.shippingAddress.lastName}
+                            </p>
+                            <p className="text-neutral-600 dark:text-neutral-300">
+                                {checkout.shippingAddress.address}
+                            </p>
+                            <p className="text-neutral-600 dark:text-neutral-300">
+                                {checkout.shippingAddress.city}, {checkout.shippingAddress.country}
+                            </p>
                         </div>
                     </div>
                 </div>
