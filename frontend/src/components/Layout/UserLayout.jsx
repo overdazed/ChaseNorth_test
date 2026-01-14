b15fdf313ca3b5642c0f66f67f79177eedb5d56d
overdazed <svetlana-pantschenko@web.de>
2026-01-14 22:41:44 +0100

    Auto-commit: 14/01/2026 22:41:44,50

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -3,7 +3,7 @@ import NewFooter from "../Common/NewFooter.jsx";
 import ParallaxSection from "../Common/ParallaxSection";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Loader from "../Common/Loader";
+import ReportLoader from "../ui/ReportLoader.jsx";
 import Newsletter from "../Common/Newsletter.jsx";
 //import Featured from '../Common/Featured.jsx';
 import FeaturesSection from "@/components/Products/FeaturesSection.jsx";
@@ -99,7 +99,7 @@ const UserLayout = () => {
             <NewFooter />
             {isLoading && (
                 <div className="fixed inset-0 bg-neutral-900 z-50">
-                    <Loader />
+                    <ReportLoader />
                 </div>
             )}
 
