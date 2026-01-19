79e7a3e18e72b036c5f19b6fdcbf82eed6267aed
overdazed <svetlana-pantschenko@web.de>
2026-01-19 10:11:43 +0100

    Auto-commit: 19/01/2026 10:11:43,85

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -387,18 +387,18 @@ const CollectionPage = () => {
                                 isDay={isDay}
                                 onFilterChange={(e) => {
                                     const { name, value, type, checked } = e.target;
-        
+    
                                     // Handle different input types
                                     if (type === 'checkbox') {
                                         const currentValues = filters[name] || [];
                                         let newValues;
-        
+    
                                         if (checked) {
                                             newValues = [...currentValues, value];
                                         } else {
                                             newValues = currentValues.filter(v => v !== value);
                                         }
-        
+    
                                         dispatch(setFilters({ ...filters, [name]: newValues }));
                                     } else if (type === 'radio') {
                                         // For radio buttons, just set the value
