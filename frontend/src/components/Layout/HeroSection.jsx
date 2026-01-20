f2db0ddbddd06ea68b7c3bdc107d18a6e8cc44e5
overdazed <svetlana-pantschenko@web.de>
2026-01-20 13:31:43 +0100

    Auto-commit: 20/01/2026 13:31:43,86

--- frontend/src/components/Layout/HeroSection.jsx
+++ frontend/src/components/Layout/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { motion } from "motion/react";
+import { Link } from "react-router-dom";
 
 export function HeroSection() {
     const heading = "dfgd Awaits dsfgdsfg sdfgsdfgsdrg sdfgsdfgsdfg sdfgsdfgsdfg";
@@ -9,7 +10,7 @@ export function HeroSection() {
     ];
 
     return (
-        <div className="relative flex h-[90vh] w-full items-center justify-center px-4">
+        <div className="relative flex h-[95vh] w-full items-center justify-center px-4">
             <div className="w-full max-w-[2000px]">
                 <h1 className="relative z-10 mx-auto max-w-6xl text-center text-2xl font-bold text-slate-700 md:text-5xl lg:text-7xl dark:text-slate-300">
                     {heading.split(" ").map((word, index) => (
@@ -19,7 +20,7 @@ export function HeroSection() {
                             animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
                             transition={{
                                 duration: 0.3,
-                                delay: index * 0.15,
+                                delay: index * 0.175,
                                 ease: "easeInOut",
                             }}
                             className="mr-2 inline-block"
@@ -38,22 +39,22 @@ export function HeroSection() {
                     {subheading}
                 </motion.p>
 
-                <div className="mt-6 space-y-4">
-                    {descriptions.map((desc, index) => (
-                        <motion.p
-                            key={index}
-                            initial={{ opacity: 0, y: 10 }}
-                            animate={{ opacity: 1, y: 0 }}
-                            transition={{
-                                duration: 0.3,
-                                delay: 0.7 + (index * 0.2)
-                            }}
-                            className="text-center text-sm text-neutral-500 dark:text-neutral-400"
-                        >
-                            {desc}
-                        </motion.p>
-                    ))}
-                </div>
+                {/*<div className="mt-6 space-y-4">*/}
+                {/*    {descriptions.map((desc, index) => (*/}
+                {/*        <motion.p*/}
+                {/*            key={index}*/}
+                {/*            initial={{ opacity: 0, y: 10 }}*/}
+                {/*            animate={{ opacity: 1, y: 0 }}*/}
+                {/*            transition={{*/}
+                {/*                duration: 0.3,*/}
+                {/*                delay: 0.7 + (index * 0.2)*/}
+                {/*            }}*/}
+                {/*            className="text-center text-sm text-neutral-500 dark:text-neutral-400"*/}
+                {/*        >*/}
+                {/*            {desc}*/}
+                {/*        </motion.p>*/}
+                {/*    ))}*/}
+                {/*</div>*/}
 
                 <motion.div
                     initial={{ opacity: 0 }}
@@ -61,9 +62,9 @@ export function HeroSection() {
                     transition={{ duration: 0.3, delay: 1.2 }}
                     className="relative z-10 mt-8 flex justify-center"
                 >
-                    <button className="w-48 transform rounded-lg bg-black px-6 py-3 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 md:w-56">
+                    <Link to="/collections/all" className="w-48 transform rounded-lg bg-black px-6 py-3 text-center font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 md:w-56">
                         Shop Now
-                    </button>
+                    </Link>
                 </motion.div>
             </div>
         </div>
